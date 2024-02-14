be759d026ca68911ddbd23b6acd699f76b914cfa
Amina Noor <159250991+Amina-codemaker3747@users.noreply.github.com>
2024-02-14 17:12:59 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,6 @@
  console.log("\tQuestion 5:\n");
 
 let famous_person = "Allama Iqbal";
- console.log(`${famous_person} said ; 
- "مجھے اے ہم نشيں رہنے دے شغل سينہ کاوی مں
-کہ ميں داغ محبت کو نماياں کر کے چھوڑوں گا
+let Quote = "I tell you the sign of a believer; When Death comes, there is a smile on his lips.";
 
-Mujhe Ae Hum Nasheen Rehne De Shughal-e-Seena Kawi Mein
-Ke Main Dagh-e-Mohabbat Ko Numayan Kar Ke Chorun Ga 
-فدا کرتا رہا دل کو حسنيeوں کی ادائوں پر
-مگر ديکھی نہ اس آئينے ميں اپنی ادا تو نے
-
-Fida Karta Raha Dil Ko Haseenon Ki Ada’on Par
-Magar Dekhi Na Iss Aaeene Mein Apni Ada Tu Ne`);
+console.log(`${famous_person} said: ${Quote}`)
